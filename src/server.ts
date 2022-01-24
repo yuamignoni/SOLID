490b0f01a4a5b05e25d4333ffd4a5acac7f871e1
yuamignoni <76108927+yuamignoni@users.noreply.github.com>
2022-01-24 17:12:24 -0300

    Starting swagger doc

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from ".";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json"
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(3333, () => console.log("Server is running!"));
